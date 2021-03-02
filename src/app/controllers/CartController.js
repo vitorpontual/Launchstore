3443bf0d2f37e7645da9ee8921ba7d40547e8789
Vitor Pontual <vitorpguedes@me.com>
2021-03-02 17:31:37 -0300

    Add Item on Cart

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -5,14 +5,33 @@ const LoadProductService = require('../services/LoadProductService')
 module.exports = {
    async index(request, response){
       try{
-	 const product = await LoadProductService.load('product', {where: { id: 5 }})
 	 let { cart } = request.session
+	 console.log(cart)
 
-	 cart = Cart.init(cart).addOne(product)
-	 return response.render('cart/index', {cart})
+	 cart = Cart.init(cart)
+
+	 return response.render('cart/index', { cart })
+
 
       }catch(err){
 	 console.error(err)
+      }
+   },
+   async addOne(request, response){
+      try{
+	 const { id } = request.params
+
+	 const product = await LoadProductService.load('product', {where: {id}})
+
+	 let { cart } = request.session 
+
+	 request.session.cart = Cart.init(cart).addOne(product)
+
+	 return response.redirect('/cart')
+      }catch(err){
+	 return response.render('cart/index', {
+	    error: "Error ao adicionar um produto"
+	 })
       }
    }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,5 +4,6 @@ const routes = express.Router()
 const CartController = require('../app/controllers/CartController')
 
 routes.get('/', CartController.index)
+routes.post('/:id/add-one', CartController.addOne)
 
 module.exports = routes
