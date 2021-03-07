088ce28c5041c87232d45e31aad751376b304fcf
Vitor Pontual <vitorpguedes@me.com>
2021-03-06 22:52:03 -0300

    Order Service

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,10 +1,10 @@
-   const LoadProductService = require('../services/LoadProductService')
+const LoadProductService = require('../services/LoadProductService')
+const LoadOrderService = require('../services/LoadOrderService')
 const User = require('../models/User')
 const Order = require('../models/Order')
 
 const mailer = require('../../lib/mailer')
 const Cart = require('../../lib/cart')
-const { formatPrice, date } = require('../../lib/utils')
 
 const email = (seller, product, buyer) => `
 <h2>Olá ${seller.name}</h2>
@@ -27,37 +27,19 @@ const email = (seller, product, buyer) => `
 
 module.exports = {
    async index(req, res){
-      let orders = await Order.findAll({where: {buyer_id: req.session.userId}})
-
-      const getOrdersPromise = orders.map(async order => {
-	 order.product = await LoadProductService.load('product', {
-	    id: order.product_id
-	 })
-	 order.buyer = await User.findOne({where: {id: order.buyer_id}})
-	 order.seller = await User.findOne({where: {id: order.seller_id}})
-	 order.formattedPrice = formatPrice(order.price)
-	 order.formattedTotal = formatPrice(order.total)
-
-	 const statuses = {
-	    open: 'Aberto',
-	    sold: 'Vendido',
-	    canceled: 'Cancelado'
-	 }
-
-	 order.formattedStatus = statuses[order.status]
-	 const updatedAt = date(order.updated_at)
-
-	 order.formattedUpdatedAt = `${order.formattedStatus} em ${updatedAt.day}/${updatedAt.month}/${updatedAt.year} às ${updatedAt.hour}:${updatedAt.minutes}`
-
-	 return order
+      const orders = await LoadOrderService.load('orders', {
+	 where: {buyer_id: req.session.userId}
       })
-
-      orders = await Promise.all(getOrdersPromise)
-			console.log(orders)
 
       return res.render('orders/index', { orders } )
    },
-  async post(req, res){
+   async sales(req, res){
+      const sales = await LoadOrderService.load('orders', {
+      where: {seller_id: req.session.userId}})
+
+      return res.render('orders/sales', {sales})
+   },
+   async post(req, res){
       try{
 	 const cart = Cart.init(req.session.cart)
 	 
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -6,6 +6,8 @@ const OrderController = require('../app/controllers/OrderController')
 const { onlyUsers } = require('../app/middleware/session')
 
 routes.get('/', onlyUsers, OrderController.index)
+   .get('/sales', onlyUsers, OrderController.sales)
+
 routes.post('/', onlyUsers, OrderController.post)
 
 module.exports = routes
